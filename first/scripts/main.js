07535dadbd50059dfdeac410d705e5ae3c217134
YanaiMaho <67141677+YanaiMaho@users.noreply.github.com>
2020-12-16 22:01:23 +0900

    a

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -1,1 +1,30 @@
 
+<script>
+      let myButton = document.querySelector('button');
+      let myHeading = document.querySelector('h1');
+      
+      function setUserName(){
+      let myName = prompt('あなたの名前を入力してください');
+        localStorage.setItem('name',myName);
+        myHeading.textContent = 'Mozilla は素晴らしいよ、' + myName;
+      }
+      
+      myButton.onclick=function(){
+      setUserName();
+      }
+      /*
+     document.querySelector('html').onclick = function(){
+     alert('痛っ！');
+     }
+     */
+     let myImage = document.querySelector('img');
+     
+     myImage.onclick=function(){
+     let mySrc = myImage.getAttribute('src');
+       if(mySrc === 'images/firefox-icon.png'){
+       myImage.setAttribute('src','images/firefox2.png');
+       }else{
+       myImage.setAttribute('src','images/firefox-icon.png');
+       }
+     }
+    </script>
